4f47398411815ba424d736c74856c075124d614a
unknown <you@example.com>
2020-09-29 01:25:53 +0200

    Updates

---
+++ precache-manifest.3b5d4c01bd326aade3a3d4932109f456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c6ed0a0152194565d3d696a7447631",
+    "url": "/http//moetez92.github.io/Aufgabe/index.html"
+  },
+  {
+    "revision": "743f3e6d91a68b021c76",
+    "url": "/http//moetez92.github.io/Aufgabe/static/css/main.729f705a.chunk.css"
+  },
+  {
+    "revision": "0d92e375ba4350b759e7",
+    "url": "/http//moetez92.github.io/Aufgabe/static/js/2.3bd152bb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/http//moetez92.github.io/Aufgabe/static/js/2.3bd152bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "743f3e6d91a68b021c76",
+    "url": "/http//moetez92.github.io/Aufgabe/static/js/main.9d083afa.chunk.js"
+  },
+  {
+    "revision": "9ca3f7aa656e5cfb3af6",
+    "url": "/http//moetez92.github.io/Aufgabe/static/js/runtime-main.28cf466f.js"
+  }
+]);
