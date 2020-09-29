984211657ada62aba6abccc5d3891fe318c56520
unknown <you@example.com>
2020-09-29 12:50:22 +0200

    delete the redux dev tools extension

--- src/store.js
+++ src/store.js
@@ -1,6 +1,7 @@
 import {createStore,applyMiddleware,compose} from "redux" ;
 import thunk from 'redux-thunk';
 import rootReducer from "./Reducers/rootReducer"
-const store = createStore(rootReducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+// const store = createStore(rootReducer,compose(applyMiddleware(thunk),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(rootReducer,compose(applyMiddleware(thunk)))
 
 export default store ;
